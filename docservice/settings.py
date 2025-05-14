4b4df19c5fe310ce132cac60230098f97dfe3ae6
Richard Saavedra <richard.saavedra@evalueserve.com>
2025-05-14 17:21:03 -0500

    agregar modelo Document y registro en Admin

--- docservice/settings.py
+++ docservice/settings.py
@@ -10,11 +10,15 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
 from pathlib import Path
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Load environment variables from .env file
+load_dotenv(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
@@ -75,8 +79,12 @@ WSGI_APPLICATION = 'docservice.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('POSTGRES_DB'),
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': os.getenv('POSTGRES_HOST', 'localhost'),
+        'PORT': os.getenv('POSTGRES_PORT', '5432'),
     }
 }
 
--- documents/models.py
+++ documents/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Document(models.Model):
+    file = models.FileField(upload_to="documents/")
+    name = models.CharField(max_length=255)
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+    size = models.PositiveIntegerField()
+    content_type = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
